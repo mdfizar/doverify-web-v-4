ee3961485428b1315cad37e3b1b48376f3aeb74f
mdfizar <mdfizarrana@gmail.com>
2024-08-26 14:24:39 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 var swiper = new Swiper(".bannarslider", {
   loop: true,
   autoplay: {
-    delay: 6000,
+    delay: 4000,
     disableOnInteraction: false,
   },
   pagination: {
@@ -10,39 +10,7 @@ var swiper = new Swiper(".bannarslider", {
     clickable: true,
   },
 });
-
-// event-news slider 
-var swiper = new Swiper(".event-news-slider", {
-  centeredSlides: true,
-  slidesPerView: 1,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-// service slider ==========
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  centeredSlides: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    300: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  },
-}); 
+  
 // features slider 
 var swiper = new Swiper(".features-slider", {
   effect: "fade",
